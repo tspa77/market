3196b60ec58297717c8835682293b28cb5de3ea7
Pavel <tspa77@gmail.com>
2018-02-14 01:53:32 +0700

    write code in branch stage-1

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 # - Поехали! (с)
+
+
+import requests
+
+def get_bot_updates():
+    url = "https://api.telegram.org/bot538702489:AAEZkKKJyaB-AYimhlxCawQ5ximbH22rIZ0/getUpdates"
+    result = requests.get(url)
+    pydict = result.json()
+    return pydict['result']
+
+
+result = get_bot_updates()
+
+
+print('У вас {} сообщений'.format(len(result)))
+answer = input("Хотите прочесть(Y/N)")
+
+if answer == 'Y':
+    for mess in range(len(result)):
+        print(result[mess]['message']['from']['first_name'],': ', result[mess]['message']['text'] )
+
+print('Сеанс окончен')
