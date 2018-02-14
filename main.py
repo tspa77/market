28c96ca62e2faf62743b7a2b704648ea74ba63f9
Pavel <tspa77@gmail.com>
2018-02-14 23:59:19 +0700

    bot responds to the last request. Manual start

--- main.py
+++ main.py
@@ -16,11 +16,25 @@ result = get_bot_updates()
 print('У вас {} сообщений'.format(len(result)))
 answer = input("Хотите прочесть(y/n)")
 
+# предлагаем посмотреть переписку бота
 if answer == 'y':
     for mess in result:
         ch_name = mess['message']['from']['first_name']
-        ch_date = datetime.datetime.fromtimestamp(mess['message']['date']).strftime('%b. %d, %I:%M')
+        ch_date = datetime.datetime.fromtimestamp(mess['message']['date']).strftime('%b. %d, %H:%M')
         ch_text = mess['message']['text']
         print('{}, {}:  {}'.format(ch_date, ch_name, ch_text))
 
-print('Сеанс окончен')
+# берем контакты последнего кто обращался
+last_chat_id = result[-1]['message']['chat']['id']
+last_chat_text = result[-1]['message']['text']
+last_chat_name = result[-1]['message']['chat']['first_name']
+
+# процедура ответа
+def send_answer(chat_id, answer_text):
+    params = {'chat_id': chat_id, 'text': answer_text}
+    respon = requests.post(bot_token + 'sendMessage', params)
+    return respon
+
+# здороваемся с последним
+answer_text = last_chat_name + ', привет!'
+send_answer(last_chat_id, answer_text)
