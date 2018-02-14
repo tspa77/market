c152f922fa39fbc4c869c12184aabbcc0964cdc2
Pavel <tspa77@gmail.com>
2018-02-14 22:11:04 +0700

    eliminated comments

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-# - Поехали! (с)
-
-
 import requests
+import datetime
+from settings import bot_token
+
 
 def get_bot_updates():
-    url = "https://api.telegram.org/bot538702489:AAEZkKKJyaB-AYimhlxCawQ5ximbH22rIZ0/getUpdates"
-    result = requests.get(url)
-    pydict = result.json()
-    return pydict['result']
-
+    # получаем ответ от АПИ
+    api_response = requests.get(bot_token + 'getUpdates')
+    # превращаем в JSON объект
+    json_response = api_response.json()
+    # берем и возвращаем ключ ["result"]  как результат работы функции
+    return json_response["result"]
 
 result = get_bot_updates()
 
-
 print('У вас {} сообщений'.format(len(result)))
-answer = input("Хотите прочесть(Y/N)")
+answer = input("Хотите прочесть(y/n)")
 
-if answer == 'Y':
-    for mess in range(len(result)):
-        print(result[mess]['message']['from']['first_name'],': ', result[mess]['message']['text'] )
+if answer == 'y':
+    for mess in result:
+        ch_name = mess['message']['from']['first_name']
+        ch_date = datetime.datetime.fromtimestamp(mess['message']['date']).strftime('%b. %d, %I:%M')
+        ch_text = mess['message']['text']
+        print('{}, {}:  {}'.format(ch_date, ch_name, ch_text))
 
 print('Сеанс окончен')
