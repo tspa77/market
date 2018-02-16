d46deba0063f469385e6c42a11610b9b656d8c32
Pavel <tspa77@gmail.com>
2018-02-16 07:16:16 +0700

    win offset

--- main.py
+++ main.py
@@ -1,52 +1,60 @@
 import requests
 import datetime
+import time
 from settings import bot_token
+# now = datetime.datetime.now()
 
-# #################
-# процедуры
-# #################
-
+# вспомогательные процедуры
 # подключение и запрос данных у бота
-def get_bot_updates():
+def get_bot_updates(offset=None):
+    params = {'offset': offset}
     # получаем ответ от АПИ
-    api_response = requests.get(bot_token + 'getUpdates')
+    api_response = requests.get(bot_token + 'getUpdates', params)
     # превращаем в JSON объект
     json_response = api_response.json()
     # берем и возвращаем ключ ["result"]  как результат работы функции
     return json_response["result"]
 
-# получение ответа от бота
+# отправка ответа от бота
 def send_answer(chat_id, answer_text):
     params = {'chat_id': chat_id, 'text': answer_text}
     respon = requests.post(bot_token + 'sendMessage', params)
     return respon
-result = get_bot_updates()
 
-# распечатка полученного массива последних сообщений
-def print_result_log():
-    for mess in result:
-        ch_name = mess['message']['from']['first_name']
-        ch_date = datetime.datetime.fromtimestamp(mess['message']['date']).strftime('%b. %d, %H:%M')
-        ch_text = mess['message']['text']
-        print('{}, {}:  {}'.format(ch_date, ch_name, ch_text))
 
-# #################
-# начало работы
-# #################
+# Самый Главный Модуль
+def main_module():
+    newoffset = None
+    while True:
+        # получаем обновления от бота
+        result = get_bot_updates(newoffset)
+        if len(result) > 0: # если есть что-то новое, то работаем дальше. Если нет - в конец вайла. Спим и курим.
 
-# здороваемся и предлагаем посмотреть переписку
-print('\n \nПриветствую тебя, о Повелитель!\nВ моём уютном чатике {} сообщений.'.format(len(result)))
-if len(result) > 0:
-        answer = input("\nХотите, я Вам прочту (y/n)")
-        if answer == 'y':
-            print_result_log()
+            # тут будет цикл, ели вдруг больше одного сообщения прилетело. Но пока работаем с одним - посленим. 
+            # берем контакты последнего кто обращался
+            last_update_id = result[-1]['update_id']
+            last_chat_text = result[-1]['message']['text']
+            last_chat_id = result[-1]['message']['chat']['id']
+            last_chat_name = result[-1]['message']['chat']['first_name']
+            
+            # реакция на сообщение
+            answer_text = last_chat_name + ', ты сказал: ' + last_chat_text + '   '
+            send_answer(last_chat_id, answer_text)
+            
+            # отладочный принт в консоль
+            print(newoffset, "  = newoffset = last_update_id   ", last_chat_text)
+            # закончили общаться с пользователем. Обновляем оффсет, идём на новый заход вайла
+            newoffset = last_update_id + 1
 
-# отклик на сообщение:
-# берем контакты последнего кто обращался
-last_chat_id = result[-1]['message']['chat']['id']
-last_chat_text = result[-1]['message']['text']
-last_chat_name = result[-1]['message']['chat']['first_name']
+        # солдат спит, служба идёт
+        time.sleep(2)
+        # отладочный принт в консоль
+        print("  Ничё нового нет, переменные не трогаю, сижу, курю, жду апдейтов   ")
 
-# здороваемся с последним
-answer_text = last_chat_name + ', привет!'
-send_answer(last_chat_id, answer_text)
+
+
+if __name__ == '__main__':  
+    try:
+        main_module()
+    except KeyboardInterrupt:
+        exit()
