4e45d23a533e70adfbfed4763a444fa092a06dbe
Pavel <tspa77@gmail.com>
2018-02-18 08:44:50 +0700

    mission completed. bonus added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import random
 from token_key import bot_token
 from settings import *
 
@@ -8,10 +9,11 @@ from settings import *
 # запрашиваем свежие курсы на криптонаторе
 def get_coins_rate():
     # проверяем, свежесть данных, если прошло меньше 1,5 минут то не обновляемся
-    # мы же не трейдеры, чё нам драконить API и бота
+    # мы же не трейдеры, чё нам драконить API и бота но зато забираю сразу все 4
+    # пары. Т.е. кэширую данные.  
+    # # Нормальный подход или так не принято?
     if time.time() - coins_rate['timestamp'] < 90:
         return coins_rate
-    
     # получаем ответ от АПИ
     api_response = requests.get(exchange_url + 'btc-usd')
     # превращаем в JSON объект
@@ -30,7 +32,7 @@ def get_coins_rate():
     coins_rate['eth-rub'] = json_response
     # временная отметка получения данных
     coins_rate['timestamp'] = time.time() 
-    # отладочный принт
+    # отладочный принт в консоль
     print("Крипта обновилась")
     return coins_rate
 
@@ -39,7 +41,6 @@ def get_bot_updates(offset=None, timeout=30):
     params = {'offset': offset, 'timeout': timeout}
     # получаем ответ от АПИ
     api_response = requests.get(bot_url + bot_token + '/getUpdates', params)
-    # отладка                   print(api_response.content)
     # превращаем в JSON объект
     json_response = api_response.json()
     # берем и возвращаем ключ ["result"] как результат работы функции
@@ -51,37 +52,57 @@ def send_answer(chat_id, answer_text):
     response = requests.post(bot_url + bot_token + '/sendMessage', params)
     return response
 
+# формируем красивый вид полученного числа
+def num_format(coins_rate, pair):
+    txt = (float(coins_rate[pair]['ticker']['price'][:-6]))
+    txt = '{0:,}'.format(txt).replace(',', ' ')
+    return txt
+
 
 # Самый Главный Модуль
 def main_module():
-    newoffset = None
+    newoffset = 891606883
     while True:
         # получаем обновления от бота
         results = get_bot_updates(newoffset)
-        for result in results: # если есть что-то новое, то работаем дальше. Если нет - в конец вайла.
+        for result in results: 
+        # если есть что-то новое, то работаем дальше. Если нет - в конец вайла.
             # берем контакты и данные из апдейта
             last_update_id = result['update_id']
-            last_chat_text = result['message']['text']
             last_chat_id = result['message']['chat']['id']
             last_chat_name = result['message']['chat']['first_name']
+
+            # Отлавливаем всякие смайлы и репосты, где нет текста
+            try:
+                last_chat_text = result['message']['text']
+            except KeyError:
+                last_chat_text = ' '
             
             # реакция на сообщение
             if last_chat_text == '/start':
                 answer_text = starttext
             elif last_chat_text == '/btcusd':
                 coins_rate = get_coins_rate()
-                answer_text = (coins_rate['btc-usd']['ticker']['price'][:-9])
+                txt = num_format(coins_rate, 'btc-usd')
+                answer_text = ('{} $ за 1 биткойн'.format(txt.replace('.', ',')))
             elif last_chat_text == '/ethusd':
                 coins_rate = get_coins_rate()
-                answer_text = (coins_rate['eth-usd']['ticker']['price'][:-9])
+                txt = num_format(coins_rate, 'eth-usd')
+                answer_text = ('{} $ за 1 эфир'.format(txt.replace('.', ',')))
             elif last_chat_text == '/btcrub':
-                coins_rate = get_coins_rate()
-                answer_text = (coins_rate['btc-rub']['ticker']['price'][:-9])
+                txt = num_format(coins_rate, 'btc-rub')
+                answer_text = ('{} ₽ за 1 биткойн'.format(txt.replace('.', ',')))
             elif last_chat_text == '/ethrub':
-                coins_rate = get_coins_rate()
-                answer_text = (coins_rate['eth-rub']['ticker']['price'][:-9])
+                txt = num_format(coins_rate, 'eth-rub')
+                answer_text = ('{} ₽ за 1 эфир'.format(txt.replace('.', ',')))
+            elif last_chat_text == '/signal':
+                answer_text = ('С вашего счёта списано 100 рублей. Рекомендация:'
+                '\n{} в созвездии {}. Ведущие астрологи {} {} {} '
+                ''.format(random.choice(planets), random.choice(zodiac), \
+                random.choice(act_1), random.choice(act_2), random.choice(coins)))
             else:
-                answer_text = (last_chat_name + ', я не понимаю по клингонски. Напиши /start и я расскажу тебе, что умею')
+                answer_text = (last_chat_name + ', я не понимаю по клингонски.'
+                '\nНапиши /start и я расскажу тебе, что умею')
 
             # отправляем ответ
             send_answer(last_chat_id, answer_text)
@@ -95,13 +116,12 @@ def main_module():
             newoffset = last_update_id + 1
 
 # Закончились вспомогательные модули - Стартуем!
-# Формируем славрь с валютами
-coins_rate = {'btc-usd': None, 'eth-usd': None, 'btc-rub': None, 'eth-rub': None, 'timestamp': 0}
+# Формируем словарь для пар
+coins_rate = {'btc-usd': None, 'eth-usd': None, 'btc-rub': None, 'eth-rub': None, 
+'timestamp': 0}
 
 # запрашиваем актальный курс
 coins_rate = get_coins_rate() 
-
-# print ('Бот запущен, курс получен. Поехали!')
 
 if __name__ == '__main__':  
     try:
